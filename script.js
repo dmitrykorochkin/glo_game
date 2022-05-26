01ca37ed0f8b1fc2df5eb25dae32c1ddf1083ab6
Dmitry Korochkin <abramslost@gmail.com>
2022-05-26 15:35:33 +0300

    Добавил замыкание

--- script.js
+++ script.js
@@ -2,58 +2,56 @@
 
 //переменная числа 
 
-const number = Math.floor(Math.random() * 100);
-const inNumber = function(num) {
-    return !isNaN(parseFloat(num)) && isFinite(num);
-}
+const numberGame = function() {
 
+    const number = Math.floor(Math.random() * 100);
+    const inNumber = function(num) {
+        return !isNaN(parseFloat(num)) && isFinite(num);
+    };
 
-
-
-
-
-    const bigNumber = function() {
-       const numberPrompt = prompt("Угадай число от 0 до 100");
-        if (numberPrompt === null) {
-            alert('Игра окончена');
-            return;
-        }
-
-
-        if (inNumber(numberPrompt)) {
-            const userNumber  = +numberPrompt;
-
-            if (userNumber > number) {
-                
-                alert('Загадочное число меньше');
-                bigNumber();
-
-            } else if (userNumber < number) {
-                alert('Загадочное число больше');
-                bigNumber();
-            } else {
-                if (confirm('Вы угадали: Сыграем еще?')) {
-                    bigNumber();
-                } else {
-                    alert('Игра окончена');
-                    return;
-                }
+    
+        const bigNumber = function() {
+        const numberPrompt = prompt("Угадай число от 0 до 100");
+            if (numberPrompt === null) {
+                alert('Игра окончена');
+                return;
             }
 
-        } else {
-            alert('Введите число');
-            bigNumber();
-        }
-            
-            
-    }; 
 
-    bigNumber();
+            if (inNumber(numberPrompt)) {
+                const userNumber  = +numberPrompt;
 
+                if (userNumber > number) {
+                    
+                    alert('Загадочное число меньше');
+                    bigNumber();
 
+                } else if (userNumber < number) {
+                    alert('Загадочное число больше');
+                    bigNumber();
+                } else {
+                    if (confirm('Вы угадали: Сыграем еще?')) {
+                        bigNumber();
+                    } else {
+                        alert('Игра окончена');
+                        return;
+                    }
+                }
 
+            } else {
+                alert('Введите число');
+                bigNumber();
+            }
+                
+                
+        }; 
 
-   
+        bigNumber();
+
+};
+
+numberGame();
+
 
 
 
