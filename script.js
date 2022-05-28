32fbc7171d30f88ad5bfc93f054924876574d112
Dmitry Korochkin <abramslost@gmail.com>
2022-05-28 08:53:39 +0300

    Правки по игре

--- script.js
+++ script.js
@@ -2,57 +2,61 @@
 
 //переменная числа 
 
-
 const number = Math.floor(Math.random() * 100) + 1;
 const inNumber = function(num) {
-        return !isNaN(parseFloat(num)) && isFinite(num);
-    };
+    return !isNaN(parseFloat(num)) && isFinite(num);
+}
 
 const numberGame = function() {
 
-        const bigNumber = function() {
+    const bigNumber = function() {
         const numberPrompt = prompt("Угадай число от 0 до 100");
-        debugger
-            if (numberPrompt === null) {
-                alert('Игра окончена');
-                return;
-            }
-
-
-            if (inNumber(numberPrompt)) {
-                const userNumber  = +numberPrompt;
-
-                if (userNumber > number) {
-                    
-                    alert('Загадочное число меньше');
-                    bigNumber();
-                    
-                } else if (userNumber < number) {
-                    alert('Загадочное число больше');
-                    bigNumber();
-                } else {
-                    if (confirm('Вы угадали: Сыграем еще?')) {
-                        bigNumber();
-                    } else {
-                        alert('Игра окончена');
-                        return;
-                    }
-                }
-
-            } else {
-                alert('Введите число');
-                bigNumber();
-            }
-                
-                
-        }; 
-
-        bigNumber();
+         if (numberPrompt === null) {
+             alert('Игра окончена');
+             return;
+         }
+ 
+ 
+         if (inNumber(numberPrompt)) {
+             const userNumber  = +numberPrompt;
+ 
+             if (userNumber > number) {
+                 
+                 alert('Загадочное число меньше');
+                 bigNumber();
+ 
+             } else if (userNumber < number) {
+                 alert('Загадочное число больше');
+                 bigNumber();
+             } else {
+                 if (confirm('Вы угадали: Сыграем еще?')) {
+                     bigNumber();
+                 } else {
+                     alert('Игра окончена');
+                     return;
+                 }
+             }
+ 
+         } else {
+             alert('Введите число');
+             bigNumber();
+         }
+             
+             
+     }; 
+ 
+     bigNumber();
+ 
 
 };
-
 numberGame();
 
+
+    
+
+
+
+   
 
 
 
